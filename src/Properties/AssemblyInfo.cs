fb251368c97d802734012ea07770bb0c5b1871db
Timothy Bruce <uniblab@hotmail.com>
2026-09-04 09:58:00 -0400

    Grant prepared regex test and benchmark access

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo( "Icod.CommandFramework.Tests" )]
+[assembly: InternalsVisibleTo( "Icod.CommandFramework.RegularExpressions.PreparedBenchmarks" )]
