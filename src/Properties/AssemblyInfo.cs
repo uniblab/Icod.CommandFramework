490437a59d8c673c6d6b180702a343748e84d7dc
Timothy Bruce <uniblab@hotmail.com>
2026-09-04 20:25:27 -0400

    Remove prepared benchmark friend access

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,4 +1,3 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo( "Icod.CommandFramework.Tests" )]
-[assembly: InternalsVisibleTo( "Icod.CommandFramework.RegularExpressions.PreparedBenchmarks" )]
